98cddafa8d2cde2a20336311ab05b403728e05f3
dkdk <konzo321@naver.com>
2025-12-08 01:53:48 +0900

    Feat: [BE] update fileExtension

--- server/src/main/java/com/example/server/business/fileExtension/domain/repository/FileExtensionRepository.java
+++ server/src/main/java/com/example/server/business/fileExtension/domain/repository/FileExtensionRepository.java
@@ -5,12 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface FileExtensionRepository extends JpaRepository<FileExtension, Integer> {
     boolean existsByExtension(String ext);
 
-    Optional<FileExtension> findByExtension(String ext);
+    FileExtension findByExtension(String ext);
 
     int countByBuiltInFalseAndEnabledFalse();
 
